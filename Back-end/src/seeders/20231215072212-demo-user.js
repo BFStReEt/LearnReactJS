393669ff36d45ab2797bd2c7dacd08e08680737e
Nguyễn Anh Tới <toidxbp02@gmail.com>
2023-12-15 15:04:11 +0700

    Day_3.1

---
+++ Back-end/src/seeders/20231215072212-demo-user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      firstName: "Toi",
+      lastName : "Nguyen",
+      email : 'toinguyen@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
